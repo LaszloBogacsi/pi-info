901df09f001f8533e6f4744586e827ee38763fb0
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2019-06-09 22:46:46 +0100

    map to temperature object on load

--- temperature_repository.py
+++ temperature_repository.py
@@ -1,3 +1,5 @@
+import psycopg2
+
 from repository import get_pool
 
 pool = get_pool
@@ -21,10 +23,28 @@ def load_all_temperature():
     conn = pool.getconn()
     if conn:
         cursor = conn.cursor()
-        cursor.execute(
-            """SELECT * FROM temperature""")
+        cursor.execute("SELECT * FROM temperature")
         all_temperature = cursor.fetchall()
+        all_temp = []
+        for temp in all_temperature:
+            all_temp.append(cast_temperature(temp, cursor))
+
         cursor.close()
         pool.putconn(conn)
         print("Put away a PostgreSQL connection")
-        return all_temperature
+        return all_temp
+
+
+class Temperature(object):
+    def __init__(self, id, temperature, sensor_status, sensor_location, published_time):
+        self.published_time = published_time
+        self.sensor_location = sensor_location
+        self.sensor_status = sensor_status
+        self.temperature = temperature
+        self.id = id
+
+
+def cast_temperature(value, cur):
+    if value is None:
+        return None
+    return Temperature(value[0], value[1], value[2], value[3], value[4])
