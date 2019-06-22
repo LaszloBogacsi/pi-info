fbd9be6eb7b28a56b6635629ae671d771a7612f3
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2019-06-22 22:00:39 +0100

    more test, tiny refactor of datamaker algo

--- data/data_normaliser.py
+++ data/data_normaliser.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from Temperature import Temperature
 
 
-def make_minute_resolution_data(data_arr):
+def make_minute_resolution_data(data_arr) -> [Temperature]:
     if len(data_arr) <= 1:
         return data_arr
 
@@ -13,15 +13,17 @@ def make_minute_resolution_data(data_arr):
         if i == len(data_arr) - 1:
             break
         second = data_arr[i + 1]
-        d_temp = second.temperature - first.temperature # can be pos and neg
-        d_time = (second.published_time - first.published_time).seconds / 60 # should be positive
-        nearest_minute = first.published_time.replace(second=0) + timedelta(minutes=1)
-        nearest_minute_temp = first.temperature + float((nearest_minute - first.published_time).seconds / 60) * d_temp / d_time
-        for j in range(int(d_time)):
+        d_temp = second.temperature - first.temperature
+        d_time_mins = (second.published_time - first.published_time).seconds / 60
+        first_time_rounded = first.published_time.replace(second=0, microsecond=0)
+        nearest_minute = first_time_rounded + timedelta(minutes=1)
+        nearest_minute_temp = first.temperature + float((nearest_minute - first_time_rounded).seconds / 60) * d_temp / d_time_mins
 
-            temp_by_minute.append(Temperature("", nearest_minute_temp + (d_temp / d_time) * j, first.sensor_status, nearest_minute + timedelta(minutes=j), first.sensor_id))
+        for j in range(int(d_time_mins)):
+            temp_by_minute.append(Temperature("", nearest_minute_temp + (d_temp / d_time_mins) * j, first.sensor_status, nearest_minute + timedelta(minutes=j), first.sensor_id))
+
     return temp_by_minute
 
 
-def get_data_for_resolution(param, resolution_mins):
+def get_data_for_resolution(data_by_minute, resolution_mins) -> [Temperature]:
     pass
--- test/DataNormaliserTest.py
+++ test/DataNormaliserTest.py
@@ -21,6 +21,26 @@ class DataNormaliserTest(unittest.TestCase):
         actual = make_minute_resolution_data(data1)
         self.assertEqual(actual, expected)
 
+    def test_can_interpolate_between_two_datapoints_with_microseconds(self):
+        data1 = [
+            Temperature( "001", 20.5, "OK", datetime.strptime("2019-06-16 10:50:30.123456", '%Y-%m-%d %H:%M:%S.%f'), 100),
+            Temperature( "001", 22.5, "OK", datetime.strptime("2019-06-16 10:52:30.456789", '%Y-%m-%d %H:%M:%S.%f'), 100),
+        ]
+
+        expected = [
+            Temperature( "", 21.0, "OK", datetime.strptime("2019-06-16 10:51:00.000000", '%Y-%m-%d %H:%M:%S.%f'), 100),
+            Temperature( "", 22.0, "OK", datetime.strptime("2019-06-16 10:52:00.000000", '%Y-%m-%d %H:%M:%S.%f'), 100)
+         ]
+        actual = make_minute_resolution_data(data1)
+        self.assertEqual(actual, expected)
+
+    def test_returns_empty_data_when_empty(self):
+        data1 = []
+
+        expected = []
+        actual = make_minute_resolution_data(data1)
+        self.assertEqual(actual, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
