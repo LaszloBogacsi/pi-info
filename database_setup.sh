30f706fd929f487d9419a8143b6d73c907ca963e
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2019-07-28 19:26:24 +0100

    update database setup

--- database_setup.sh
+++ database_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-psql -U postgres
+sudo -u postgres psql
 CREATE DATABASE haut;
 CREATE USER lbhautpguser WITH ENCRYPTED PASSWORD 'u!6[l;$K;q$$ST&U';
 GRANT ALL PRIVILEGES ON DATABASE haut TO lbhautpguser;
