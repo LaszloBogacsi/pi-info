38b5e073dbbc101a3186ad014236f34e5712a568
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2019-07-28 19:53:44 +0100

    revert move app.py

--- pi_info/__main__.py
+++ pi_info/__main__.py
@@ -1,18 +1,4 @@
-from flask import Flask
-
-from pi_info.blueprints.home import home
-from pi_info.blueprints.lights import lights
-from pi_info.blueprints.rooms import rooms
-from pi_info.blueprints.sensors import sensors
-from pi_info.blueprints.tube_status import tube_status
-
-
-app = Flask(__name__)
-app.register_blueprint(home)
-app.register_blueprint(lights)
-app.register_blueprint(sensors)
-app.register_blueprint(tube_status)
-app.register_blueprint(rooms)
+from pi_info.app import app
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9080)
