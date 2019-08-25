83aa5dab7b0d21fdda11d36a47b6ed17cc80e5fe
Laszlo Bogacsi <laszlo.bogacsi@itv.com>
2019-08-25 13:42:18 +0100

    fix sidebar and sensor card responsiveness

--- pi_info/data/sensors.py
+++ pi_info/data/sensors.py
@@ -15,8 +15,9 @@ class SensorCode(Enum):
 
 LIVING_ROOM1 = {"sensor_id": 100, "name": "Living Room 1", "location": Room.LIVING_ROOM, "code": SensorCode.DHT22, "type": SensorType.TEMPERATURE_AND_HUMIDITY, "sampling_rate_mins": 20}
 MASTER_BEDROOM1 = {"sensor_id": 101, "name": "Master BedRoom 1", "location": Room.MASTER_BEDROOM, "code": SensorCode.DHT22, "type": SensorType.TEMPERATURE_AND_HUMIDITY, "sampling_rate_mins": 20}
+MASTER_BEDROOM2 = {"sensor_id": 101, "name": "Master BedRoom 1", "location": Room.MASTER_BEDROOM, "code": SensorCode.DHT22, "type": SensorType.TEMPERATURE_AND_HUMIDITY, "sampling_rate_mins": 20}
 
-SENSORS = [LIVING_ROOM1, MASTER_BEDROOM1]
+SENSORS = [LIVING_ROOM1, MASTER_BEDROOM1, MASTER_BEDROOM2, MASTER_BEDROOM2, MASTER_BEDROOM2]
 
 
 def get_sensor_by_id(sensor_id):
