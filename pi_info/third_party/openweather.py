5f5654a10ce5c65a1016da2ea9c2925c0142f0be
Laszlo Bogacsi <bogacsi.laszlo@gmail.com>
2019-10-08 23:29:51 +0100

    enable weather

--- pi_info/third_party/openweather.py
+++ pi_info/third_party/openweather.py
@@ -1,3 +1,6 @@
+import json
+from urllib import request
+
 api_key = "92334de0a0f6939c969570a910190823"
 north_acton = {
     "lat": "51.523134",
@@ -27,11 +30,11 @@ def create_current_weather(data):
 
 def get_current_weather_info() -> [CurrentWeather]:
     try:
-        # with request.urlopen(url=url) as response:
-        #     data = response.read()
-        #     json_weather_data = json.loads(data)
-        #     return create_current_weather(json_weather_data)
-        return None
+        with request.urlopen(url=url) as response:
+            data = response.read()
+            json_weather_data = json.loads(data)
+            return create_current_weather(json_weather_data)
+
     except:
         print("could not connect to openweather api")
         return None
